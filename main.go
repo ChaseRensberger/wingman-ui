2d213e42c3852edbe998b395698d3055a0aaf94c
Chase Rensberger <chasehrensberger@gmail.com>
2026-03-23 19:55:05 -0400

    initial components

--- main.go
+++ main.go
@@ -9,20 +9,16 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type pageData struct {
-	TestValue int
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl := template.Must(
+		template.Must(template.ParseFiles("index.html")).ParseGlob("components/*.html"),
+	)
 	r.Handle("/output.css", http.FileServer(http.Dir(".")))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, pageData{
-			TestValue: 1,
-		})
+		tmpl.Execute(w, nil)
 	})
 
 	log.Fatal(http.ListenAndServe(":3000", r))
